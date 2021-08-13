487c5e0aec4e61cd3dd1927890586931558dff93
Γεώργιος Αλεξάκης <58263228+Looper2074@users.noreply.github.com>
2021-08-13 17:56:31 +0300

    Update testing_train_decolle.sh

--- DECOLLE/training/testing_train_decolle.sh
+++ DECOLLE/training/testing_train_decolle.sh
@@ -7,6 +7,15 @@ output_path="../output_data"
 #params_dvsgestures_01.yml
 #--save_dir=
 #--verbose=True to display the parameters we are using
-
-python $path/scripts/train_lenet_decolle.py --params_file=$path/scripts/parameters/params_dvsgestures_01.yml --save_dir=$output_path/01
-
+parameters_to_check=(01 02 03 04 05 06 07)
+for t in ${parameters_to_check[@]}; do
+    mkdir ./plots/bar/$t
+    mkdir ./plots/confusion_matrices/$t
+    mkdir ./plots/correct_perc/$t
+    mkdir ./plots/test_loss/$t
+    mkdir ./plots/total_loss/$t
+done
+for t in ${parameters_to_check[@]}; do
+    python $path/scripts/train_lenet_decolle.py --params_file=$path/scripts/parameters/params_dvsgestures_${t}.yml --save_dir=$output_path/$t
+done
+python plot_results.py
