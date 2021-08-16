340a21952f546f384d4827e6352b20c968b66e0a
Γεώργιος Αλεξάκης <58263228+Looper2074@users.noreply.github.com>
2021-08-16 16:20:06 +0300

    Update testing_train_decolle.sh

--- DECOLLE/training/testing_train_decolle.sh
+++ DECOLLE/training/testing_train_decolle.sh
@@ -7,7 +7,7 @@ output_path="../output_data"
 #params_dvsgestures_01.yml
 #--save_dir=
 #--verbose=True to display the parameters we are using
-parameters_to_check=(01 02 03 04 05 06 07)
+parameters_to_check=(01 02 03 04 05 06 07 08 09 10 11 12 13 14)parameters_to_check=(01)
 for t in ${parameters_to_check[@]}; do
     mkdir ./plots/bar/$t
     mkdir ./plots/confusion_matrices/$t
@@ -16,6 +16,12 @@ for t in ${parameters_to_check[@]}; do
     mkdir ./plots/total_loss/$t
 done
 for t in ${parameters_to_check[@]}; do
+    echo "Starting training with parameters $t"
     python $path/scripts/train_lenet_decolle.py --params_file=$path/scripts/parameters/params_dvsgestures_${t}.yml --save_dir=$output_path/$t
+    ---------------Epoch 1-------------
+    echo "Finished training with parameters $t"
+    echo "Plotting training results for parameters $t"
+    python plot_results.py $t
+    echo "Finished plotting training results for parameters $t"
 done
-python plot_results.py
+
