92fe351e5fe51d1dcc8bef45157812b85631bb7c
Γεώργιος Αλεξάκης <58263228+Looper2074@users.noreply.github.com>
2021-09-07 17:56:07 +0300

    Add files via upload

--- neko/neko/poker.py
+++ neko/neko/poker.py
@@ -4,7 +4,7 @@ import tonic
 import tonic.transforms as transforms
 import numpy as np
 import torch
-
+from tqdm import tqdm
 from .backend import numpy_backend as npb
 from .utils.network_utils import download_file
 
@@ -37,25 +37,30 @@ class MNIST(Dataset):
         """
 
 
-        transform = transforms.Compose([transforms.ToSparseTensor()])
-        train = tonic.datasets.POKERDVS(save_to='../data/POKERDVS',train=True,download=False)#,transform=transform)
-        test = tonic.datasets.POKERDVS(save_to='../data/POKERDVS',train=False,download=False)#,transform=transform)
-        x_train, x_test, y_train, y_test = [], [], [], []
-        for i in range(len(train)):
+        transform = transforms.Compose([
+        tonic.transforms.Downsample( time_factor=0.00001)])
+        train = tonic.datasets.DVSGesture(save_to='../data/dvsgesture/',train=True,download=False)#,transform=transform)
+        test = tonic.datasets.DVSGesture(save_to='../data/dvsgesture/',train=False,download=False)#,transform=transform)
+        y_train, y_test = np.array([]), np.array([])
+        x_train = np.array([train[0][0][0:10000:1000]])
+        x_test = np.array([test[0][0][0:10000:1000]])
+        for i in tqdm(range(len(train))):
             c = 0
-            while c+500 < len(train[i][0]):
-                x_train.append(train[i][0][c:500+c].tolist())
-                y_train += [train[i][1]]
-                c = c + 500
-        for i in range(len(test)):
+            while c+10000 < len(train[i][0]):
+                x_train = np.append(x_train,[train[i][0][c:10000+c:1000]],axis=0)
+                y_train = np.append(y_train,[train[i][1]])
+                c = c + 10000
+        for i in tqdm(range(len(test))):
             c = 0
-            while c+500 < len(test[i][0]):
-                x_test.append(test[i][0][c:c+500].tolist())
-                y_test += [test[i][1]]
-                c = c + 500
+            while c+10000 < len(test[i][0]):
+                x_test = np.append(x_test, [test[i][0][c:10000 + c:1000]],axis=0)
+                y_test = np.append(y_test,[test[i][1]])
+                c = c + 10000
         x_train, x_test, y_train, y_test = np.asarray(x_train), np.asarray(x_test), np.asarray(y_train), np.asarray(y_test)
         x_train, x_test = x_train.astype(np.float32), x_test.astype(np.float32)
-        y_train = npb.categorical_to_onehot(y_train, 4).astype(np.float32)
-        y_test = npb.categorical_to_onehot(y_test, 4).astype(np.float32)
-        print(x_train.shape)
+        x_train, x_test = x_train[1:], x_test[1:]
+        y_train = y_train.astype(int)
+        y_test = y_test.astype(int)
+        y_train = npb.categorical_to_onehot(y_train, 11).astype(np.float32)
+        y_test = npb.categorical_to_onehot(y_test, 11).astype(np.float32)
         return x_train, y_train, x_test, y_test
